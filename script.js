a0ad584a963eab401b458e4f675404d8b65689ce
xprstaleic34 <xprstaleic34@gmail.com>
2026-03-04 20:28:52 +0100

    Add files via upload
    Signed-off-by: xprstaleic34 <xprstaleic34@gmail.com>

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Optional: hier könnt ihr später dynamisch den Spielnamen setzen
+// document.getElementById("gameName").textContent = "Euer Spielname";
